16d52ede337e2551b7d4926ef634504a3bcf560a
Mangesh Jogade <mangesh.mpj@gmail.com>
2017-12-23 21:19:50 -0600

    Added list components

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,10 +1,35 @@
 var React = require('react');
+var ToDoList = require('ToDoList');
 
 var ToDoApp = React.createClass({
+    getInitialState: function () {
+      return {
+        todos : [
+          {
+            id: 1,
+            text: 'walk the dog'
+          },
+          {
+            id: 2,
+            text: 'Clean the yard'
+          },
+          {
+            id: 3,
+            text: 'Clean the yard 1'
+          },
+          {
+            id: 4,
+            text: 'Clean the yard 2'
+          }
+        ]
+      };
+    },
     render: function() {
+      var {todos} = this.state;
+
       return (
         <div>
-          ToDo App JSX
+          <ToDoList todos={todos}/>
         </div>
       )
     }
