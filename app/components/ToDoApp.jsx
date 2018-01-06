e3429e4a0ae7067bb98a71f8e76a4b4858efbe42
Mangesh Jogade <mangesh.mpj@gmail.com>
2018-01-06 14:28:24 -0600

    Added search components and other changes

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,9 +1,13 @@
 var React = require('react');
 var ToDoList = require('ToDoList');
+var AddToDo = require('AddToDo');
+var TodoSearch = require('TodoSearch');
 
 var ToDoApp = React.createClass({
     getInitialState: function () {
       return {
+        showCompleted: false,
+        searchText:'',
         todos : [
           {
             id: 1,
@@ -24,12 +28,24 @@ var ToDoApp = React.createClass({
         ]
       };
     },
+
+    handleSearch: function (showCompleted, searchText) {
+      this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+      });
+    },
+    handleAddTodo: function (text) {
+      alert(text);
+    },
     render: function() {
       var {todos} = this.state;
 
       return (
         <div>
+          <TodoSearch onSearch={this.handleSearch}/>
           <ToDoList todos={todos}/>
+          <AddToDo onAddTodo={this.handleAddTodo}/>
         </div>
       )
     }
